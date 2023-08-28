8005af099407cd4dfa0d84282f471eb4b74801fa
Pedro Henrivas <pedrohenrivas@gmail.com>
2023-08-28 14:18:35 -0300

    Atualizado

---
+++ Sintaxe Básica em JavaScript/aula 02 - variaveis/script.js
@@ -0,0 +1,102 @@
+// Tipos primitivos
+
+// // boolean
+// var vOuF = false
+// console.log(typeof(vOuF));
+
+// // number
+// var numeroQualquer = 1;
+// console.log(typeof(numeroQualquer));
+
+// // string
+// var nome = 'Pedro';
+// console.log(typeof(nome));
+
+// // function
+// var funcao = function() {}
+// console.log(typeof(funcao));
+
+// como declarar
+// var variavel = 'Pedro';
+// variavel = 'Henrique';
+// console.log(variavel);
+
+// let variavel2 = 'Pedro';
+// console.log(variavel2);
+
+// const = não pode ser reatribuida
+// const constate = 'Pedro';
+// console.log(constante)
+
+// var escopoGlobal = 'global';
+// console.log(escopoGlobal)
+
+// function escopoLocal () {
+//     let escopoLocalInterno = 'local';
+//     console.log(escopoLocalInterno);
+// }
+
+// escopoLocal ();
+
+// atribuição
+
+// var atribuicao = 'Pedro';
+
+// // comparação
+// var comparacao = '0' == '0';
+// console.log(comparacao);
+
+// // comparação identica
+// var comparacaoIdentica = '0' === 0;
+// console.log(comparacaoIdentica);
+
+// // adição
+// var adicao = 1 + 1;
+// console.log(adicao);
+
+// // subtração
+// var subtracao = 2 - 1;
+// console.log(subtracao);
+
+// // multiplicação
+// var multiplicacao = 3 * 3;
+// console.log(multiplicacao);
+
+// // divisão real
+// var divisao = 6 / 2;
+// console.log(divisao);
+
+// // divisão inteira
+// var divisaoInteira = 5 % 2;
+// console.log(divisaoInteira);
+
+// // potenciação
+// var potenciacao = 2 ** 2;
+// console.log(potenciacao);
+
+// // operadores relacionais
+// // maior que
+// var maiorQue = 3 > 2;
+// console.log(maiorQue);
+
+// // menor que
+// var menorque = 3 < 2;
+// console.log(menorque);
+
+// // maior ou igual que
+// var maiorOuIgual = 3 >= 2;
+// console.log(maiorOuIgual);
+
+// // menor ou igual que
+// var menorOuIgual = 3 <= 2;
+// console.log(menorOuIgual);
+
+// Operadores Lógicos
+var e = true && true;
+console.log(e);
+
+var ou = false || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);
