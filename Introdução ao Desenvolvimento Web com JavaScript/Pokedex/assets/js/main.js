1d524ec6364d69ebe3415f668282d9a92656ff49
Pedro Henrivas <pedrohenrivas@gmail.com>
2023-09-01 10:28:42 -0300

    Atualizado

--- Introdução ao Desenvolvimento Web com JavaScript/Pokedex/assets/js/main.js
+++ Introdução ao Desenvolvimento Web com JavaScript/Pokedex/assets/js/main.js
@@ -1,20 +1,17 @@
 
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 function convertPokemonToLi(pokemon) {
     return ` 
         <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             
             <div class="detail">
                 <ol class="types">
-                    ${convertPokemonTypesToLi(pokemon.types).join('')}
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+                <img src="${pokemon.photo}" 
+                        alt="${pokemon.name}">
             </div>
         </li>
     
@@ -24,5 +21,6 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
 })
--- Introdução ao Desenvolvimento Web com JavaScript/Pokedex/assets/js/poke-api.js
+++ Introdução ao Desenvolvimento Web com JavaScript/Pokedex/assets/js/poke-api.js
@@ -1,7 +1,25 @@
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+    
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then((respoense) => respoense.json())
+    return fetch(pokemon.url)
+            .then((respoense) => respoense.json())
+            .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
@@ -14,13 +32,3 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonDetails) => pokemonDetails)
 }
-
-
-Promise.all([
-    fetch ('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch ('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch ('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch ('https://pokeapi.co/api/v2/pokemon/4')
-]).then((results) => {
-    console.log(results)
-})
