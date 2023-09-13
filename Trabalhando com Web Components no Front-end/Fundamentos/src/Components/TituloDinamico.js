d05695977358392bd0835b2f5a55db36aa1d48de
Pedro Henrivas <pedrohenrivas@gmail.com>
2023-09-13 12:24:19 -0300

    atualizado

--- Trabalhando com Web Components no Front-end/Fundamentos/src/Components/TituloDinamico.js
+++ Trabalhando com Web Components no Front-end/Fundamentos/src/Components/TituloDinamico.js
@@ -7,7 +7,7 @@ class TituloDinamico extends HTMLElement{
         //Base do component
 
         const componentRoot = document.createElement('h1');
-        componentRoot.textContent = 'Minha notícia'
+        componentRoot.textContent = this.getAttribute("titulo");
 
         //estilizar component
 
